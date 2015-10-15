4cf45e430a98bbad5877bbff616f7f32d6b09513
ruchva <ru.develop.net@hotmail.es>
2015-10-15 08:41:25 -0400

    consultas utiles.sql

---
+++ consultas utiles.sql
@@ -0,0 +1,39 @@
+--resta fechas y las muestra en años y meses
+select DATEDIFF(MONTH,'2006-11-01 00:00:00',getdate()), DATEDIFF(MONTH,'2006-11-01 00:00:00',getdate()) / 12, DATEDIFF(MONTH,'2006-11-01 00:00:00',getdate()) % 12 
+select DATEDIFF(MONTH,'1995-10-07 00:00:00',getdate()), DATEDIFF(MONTH,'1995-10-07 00:00:00',getdate()) / 12, DATEDIFF(MONTH,'1995-10-07 00:00:00',getdate()) % 12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
